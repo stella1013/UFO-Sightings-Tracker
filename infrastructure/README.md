362016cc17d1911b84a6541f11ea66438504691f
Veronica Preston <veronica.preston@bioscapedigital.com>
2023-09-10 15:51:22 -0400

    build(infra): add security group and cloud variables

---
+++ infrastructure/README.md
@@ -0,0 +1,17 @@
+# Terraform
+
+## Step 0: Initialize Terraform
+
+terraform init
+
+## Step 1: Plan Resources
+
+terraform plan -var-file="variables-us-east-2.tfvars"
+
+## Step 2: Apply Resources
+
+terraform apply -var-file="variables-us-east-2.tfvars"
+
+## Step 3: Destroy Resources
+
+terraform apply -var-file="variables-us-east-2.tfvars"
--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -15,7 +15,28 @@ terraform {
   }
 
 }
+variable "UFOST_AWS_ACCESS_KEY_ID" {
+    type = string
 
+}
+variable "UFOST_AWS_ACCESS_KEY" {
+     type = string
+}
+variable "aws_region" {
+     type = string
+}
+variable "instance_name" {
+     type = string
+}
+variable "vpc_id" {
+     type = string
+}
+variable "vpc_cidr_block" {
+     type = string
+}
+variable "key_name" {
+     type = string
+}
 provider "aws" {
   region = var.aws_region
 }
@@ -41,4 +62,47 @@ resource "aws_iam_role" "role" {
   name               = "test_role"
   path               = "/"
   assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+resource "aws_instance" "app-server-jenkins" {
+  ami             = "ami-00a9282ce3b5ddfb1"
+  instance_type   = "t2.micro"
+  key_name = var.key_name
+  iam_instance_profile = "${aws_iam_instance_profile.test_profile.name}"
+  vpc_security_group_ids = [aws_security_group.jenkins_secgroup.id]
+  
+  tags = {
+    Name = var.instance_name
+  }
+}
+
+resource "aws_security_group" "jenkins_secgroup" {
+  name        = "jenkins_secgroup"
+  description = "Allow Jenkins web traffic for inbound ssh and http and all outbound"
+  vpc_id      = var.vpc_id
+  
+
+  ingress {
+    description = "Allow from Personal CIDR block"
+    from_port   = 8081
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow SSH from Personal CIDR block"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
 }
